2a62a1e8537b4943538934decdeeaad15f4c31c5
Avinaba-Ray <avinabaray@gmail.com>
2020-01-28 01:38:12 +0530

    Emojis added in Result

--- app/src/main/java/mohit/codex_iter/www/awol/ResultAdapter.java
+++ app/src/main/java/mohit/codex_iter/www/awol/ResultAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,6 +41,20 @@ public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.ResultHold
         holder.creditsTextView.setText(resultData.get(position).getTotalearnedcredit());
         holder.sgpaTextView.setText(resultData.get(position).getSgpaR());
 
+        double currSGPA = Double.parseDouble(resultData.get(position).getSgpaR());
+        if (currSGPA>=8.5 && currSGPA<=10.0) {
+            //excellent
+            holder.imageViewResultEmotion.setImageResource(R.drawable.ic_excellent);
+        } else if (currSGPA>=7.0 && currSGPA<8.5) {
+            //good
+            holder.imageViewResultEmotion.setImageResource(R.drawable.ic_good);
+        } else if (currSGPA>=5.0 && currSGPA<7.0) {
+            //average
+            holder.imageViewResultEmotion.setImageResource(R.drawable.ic_average);
+        } else {
+            //poor
+            holder.imageViewResultEmotion.setImageResource(R.drawable.ic_poor);
+        }
         holder.recyclerResultParent.setOnClickListener(view -> {
             if (listener != null) {
                 listener.onResultClicked(resultData.get(position).getStynumber(), resultData.get(position).getTotalearnedcredit(),
@@ -58,6 +73,7 @@ public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.ResultHold
 //        CardView cardView;
         TextView semTextView, sgpaTextView, creditsTextView;
         LinearLayout recyclerResultParent;
+        ImageView imageViewResultEmotion;
 
         public ResultHolder(@NonNull View itemView) {
             super(itemView);
@@ -66,6 +82,7 @@ public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.ResultHold
             semTextView = itemView.findViewById(R.id.textViewSem);
             creditsTextView = itemView.findViewById(R.id.textViewCredits);
             sgpaTextView = itemView.findViewById(R.id.textViewSGPA);
+            imageViewResultEmotion = itemView.findViewById(R.id.imageViewResultEmotion);
         }
     }
     public interface OnItemClickListener {
