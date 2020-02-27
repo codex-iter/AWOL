48ac4036834d7de991fdb28b78b7273e34770c8d
Avinaba-Ray <avinabaray@gmail.com>
2020-02-27 17:43:50 +0530

    App Data cleared programatically

---
+++ app/src/main/java/mohit/codex_iter/www/awol/MyApplication.java
@@ -0,0 +1,49 @@
+package mohit.codex_iter.www.awol;
+import android.app.Application;
+
+import java.io.File;
+
+public class MyApplication extends Application {
+
+    private static MyApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    public static MyApplication getInstance() {
+        return instance;
+    }
+
+    public void clearApplicationData() {
+        File cacheDirectory = getCacheDir();
+        File applicationDirectory = new File(cacheDirectory.getParent());
+        if (applicationDirectory.exists()) {
+            String[] fileNames = applicationDirectory.list();
+            for (String fileName : fileNames) {
+                if (!fileName.equals("lib")) {
+                    deleteFile(new File(applicationDirectory, fileName));
+                }
+            }
+        }
+    }
+
+    public static boolean deleteFile(File file) {
+        boolean deletedAll = true;
+        if (file != null) {
+            if (file.isDirectory()) {
+                String[] children = file.list();
+                for (int i = 0; i < children.length; i++) {
+                    deletedAll = deleteFile(new File(file, children[i])) && deletedAll;
+                }
+            } else {
+                deletedAll = file.delete();
+            }
+        }
+
+        return deletedAll;
+    }
+
+}
